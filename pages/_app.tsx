f35c71d8d40f54c553a33e21625519dbaaeb19cd
GetzInsomnia <get-insomnia@live.com>
2025-07-19 02:05:25 +0700

    fix pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { NextIntlProvider } from 'next-intl';
+import { IntlProvider } from 'next-intl';
 import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { locale, defaultLocale } = useRouter();
   return (
-    <NextIntlProvider
-      locale={locale}
+    <IntlProvider
+      locale={locale!}
       messages={pageProps.messages}
       defaultLocale={defaultLocale}
     >
       <Component {...pageProps} />
-    </NextIntlProvider>
+    </IntlProvider>
   );
 }
