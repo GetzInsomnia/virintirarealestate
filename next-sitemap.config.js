2c98751ca8d104356c0c3d2cab8fa1ebb296c0b3
Pruttibodee Adam <154861856+GetzInsomnia@users.noreply.github.com>
2025-07-20 15:20:02 +0700

    Add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://your-domain.com',
+  generateRobotsTxt: true,
+  i18n: {
+    defaultLocale: 'th',
+    locales: ['th', 'en'],
+  },
+};
