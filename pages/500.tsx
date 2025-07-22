0d6fce67f0305e5836a9e0ba1e7e42b7fdad115c
Pruttibodee Adam <154861856+GetzInsomnia@users.noreply.github.com>
2025-07-22 22:21:42 +0700

    Add custom 500 error page with translations

---
+++ pages/500.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+import { GetStaticProps } from "next";
+import nextI18NextConfig from "../next-i18next.config";
+
+export default function Custom500() {
+  const { t } = useTranslation("common");
+  const { asPath } = useRouter();
+  const lang = asPath.split("/")[1] || nextI18NextConfig.i18n.defaultLocale;
+  const ogLocale = lang === "en" ? "en_US" : "th_TH";
+  return (
+    <>
+      <NextSeo
+        title={`500 - ${t("seo_title")}`}
+        openGraph={{ locale: ogLocale }}
+      />
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        <h1>500</h1>
+        <p>{t("internalError")}</p>
+        <p>
+          <a href={`/${lang}`}>{t("backHome")}</a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(
+      locale ?? nextI18NextConfig.i18n.defaultLocale,
+      ["common"]
+    )),
+  },
+});
