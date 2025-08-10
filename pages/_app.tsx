2fde228bd7b82a89eca29a1d19db5dd9429b656e
Pruttibodee Adam <154861856+GetzInsomnia@users.noreply.github.com>
2025-08-10 21:58:28 +0700

    Add Organization and WebSite JSON-LD

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,45 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { appWithTranslation } from 'next-i18next'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
+import defaultSeo from "../next-seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const baseUrl = defaultSeo.baseUrl;
+  const orgJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Virintira",
+    url: baseUrl,
+    logo: `${baseUrl}/favicon.ico`,
+    sameAs: [
+      "https://twitter.com/virintira",
+      "https://www.facebook.com/virintira",
+    ],
+  };
+
+  const webSiteJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "Virintira",
+    url: baseUrl,
+  };
+
+  return (
+    <>
+      <Script
+        id="organization-jsonld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }}
+      />
+      <Script
+        id="website-jsonld"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(webSiteJsonLd) }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
