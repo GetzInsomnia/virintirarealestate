f46198e240f31c553bf97fe963f194f5b68395f5
techbiz-solution <techbiz.solution2024@gmail.com>
2025-09-16 08:42:42 +0700

    Update contact channels and icons

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -6,8 +6,8 @@ export interface ContactConfig {
 }
 
 export const CONTACT: ContactConfig = {
-  lineOA: 'https://line.me/ti/p/@zomzomproperty',
-  facebook: 'https://www.facebook.com/zomzomproperty',
-  tiktok: '',
-  phone: '+66987654321',
+  lineOA: '',
+  facebook: '',
+  tiktok: 'https://www.tiktok.com/@zomzomproperty',
+  phone: '',
 }
